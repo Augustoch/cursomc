db87084ee6c61cc8a2eea132aba63de3dbf2f107
Augusto Cavalcante <joseaugustocavalcanteholanda@gmail.com>
2019-12-08 09:27:36 -0300

    domain categoria

--- src/main/java/br/com/augusto/resources/CategoriaResources.java
+++ src/main/java/br/com/augusto/resources/CategoriaResources.java
@@ -1,16 +1,30 @@
 package br.com.augusto.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.augusto.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@GetMapping
-	public String listar() {
-		return "IT IS OK";
+	public List<Categoria> listar() {
+		
+		Categoria infor = new Categoria(1, "Informática");
+		Categoria escrit = new Categoria(2, "Escritório"); 
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(escrit);
+		categorias.add(infor);
+		
+		
+		return categorias;
 	}
 	
 }
